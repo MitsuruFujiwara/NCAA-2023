8adb0a20cb4b8e382959c74fd9c4dab6c57c3693
MitsuruFujiwara <fujiwara52jp@gmail.com>
2023-02-24 14:38:55 +0900

    update seasons

--- src/004_seasons.py
+++ src/004_seasons.py
@@ -4,16 +4,19 @@ import pandas as pd
 import sys
 
 from utils import save2pkl, line_notify
-from utils import MBASE_DIR
 
 #==============================================================================
-# preprocess seasons mens
+# preprocess seasons
 #==============================================================================
 
 def main():
 
     # load csv
-    Seasons = pd.read_csv(f'{MBASE_DIR}/MSeasons.csv')
+    MSeasons = pd.read_csv('../input/MSeasons.csv')
+    WSeasons = pd.read_csv('../input/WSeasons.csv')
+
+    # merge
+    Seasons = pd.concat([MSeasons,WSeasons])
 
     # drop team name
     Seasons.drop('DayZero',axis=1,inplace=True)
@@ -23,7 +26,7 @@ def main():
         Seasons[r] = Seasons[r].map(Seasons[r].value_counts())
 
     # save pkl
-    save2pkl('../feats/seasons_mens.pkl', Seasons)
+    save2pkl('../feats/seasons.pkl', Seasons)
 
     # LINE notify
     line_notify('{} done.'.format(sys.argv[0]))
